64105c3746dc18965e7609a5c3e531d36340a630
JoshDaigleDev <joshwdaigle@gmail.com>
2023-09-30 23:50:46 -0500

    khjgb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-72">
       <p>Adna Is Short.</p>
+      
     </main>
   )
 }
