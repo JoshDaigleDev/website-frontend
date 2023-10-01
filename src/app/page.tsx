e2d3b37b74c8c09340dbaefaaa13c3c6acfc0b7f
JoshDaigleDev <joshwdaigle@gmail.com>
2023-10-01 00:33:04 -0500

    love

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-72">
-      <p>Adna Is Short.</p>
+      <p>I love Adna</p>
     </main>
   )
 }
